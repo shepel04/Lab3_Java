f5ace107d95bb27cbc769b157016999bb8c91855
shepel04 <95418153+shepel04@users.noreply.github.com>
2024-03-29 14:20:03 +0200

    Initial commit

---
+++ src/Manager.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+
+public class Manager {
+
+    public Semaphore access;
+    public Semaphore full;
+    public Semaphore empty;
+
+    public ArrayList<String> storage = new ArrayList<>();
+
+    public Manager(int storageSize) {
+        access = new Semaphore(1);
+        full = new Semaphore(storageSize);
+        empty = new Semaphore(0);
+    }
+}
