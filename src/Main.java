02732de1c946246b7f3f7f5fa97417f85f1a55ef
shepel04 <95418153+shepel04@users.noreply.github.com>
2024-04-03 19:24:18 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -6,28 +6,28 @@ public class Main {
         Main main = new Main();
         int storageSize = 10;
         int itemNumbers = 30;
-        main.starter(storageSize, itemNumbers);
+        int producerCount = 3;
+        int consumerCount = 5;
+        main.starter(storageSize, itemNumbers, producerCount, consumerCount);
     }
 
-    private void starter(int storageSize, int itemNumbers) {
+    private void starter(int storageSize, int itemNumbers, int producerCount, int consumerCount) {
         Manager manager = new Manager(storageSize);
-        int item = 0;
-        int i = 0;
-        while (item < itemNumbers) {
-            int col = (int) Math.round(Math.random() * storageSize);
-            if (itemNumbers - item > col) {
-                new Producer(col, manager, i);
-                new Consumer(col, manager, i);
-                item += col;
-            } else {
-                new Producer(itemNumbers - item, manager, i);
-                new Consumer(itemNumbers - item, manager, i);
-                break;
-            }
-            i += 1;
-            if (i == 20) {
-                break;
-            }
+        int remainingItems = itemNumbers;
+        //Random random = new Random();
+
+        for (int i = 0; i < producerCount && remainingItems > 0; i++) {
+            int producedItems = Math.min(storageSize, remainingItems);
+            new Producer(producedItems, manager, i);
+            remainingItems -= producedItems;
+        }
+
+        remainingItems = itemNumbers;
+
+        for (int i = 0; i < consumerCount && remainingItems > 0; i++) {
+            int consumedItems = Math.min(storageSize, remainingItems);
+            new Consumer(consumedItems, manager, i);
+            remainingItems -= consumedItems;
         }
     }
 }
--- src/Producer.java
+++ src/Producer.java
@@ -29,4 +29,6 @@ public class Producer implements Runnable{
             }
         }
     }
+
+
 }
